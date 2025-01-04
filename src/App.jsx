8dd16644fe9b639a27eb6bf5a379e86790579dee
Swarnali Bhattacharyya <oli2k3@gmail.com>
2025-01-04 20:57:25 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-italic">
+    <h1 className="text-5xl font-italic">
       Hello world!!!!!!
     </h1>
   )
