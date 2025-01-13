198434957cdbaf181260f4cdc4799dc63b0c0fda
Swarnali Bhattacharyya <oli2k3@gmail.com>
2025-01-13 21:05:36 +0530

    refactor Button component to include SVG and enhance styling options

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import ButtonSvg from '../assets/svg/ButtonSvg'
+const Button = ({className,href,onclick,children,px,white}) => {
+  const classes = `button relative infline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || 'px-5'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''}`;
+  const RenderButton = () => (
+    <button className={classes}>
+        <span>{children}</span>
+        {ButtonSvg(white)}
+    </button>
+  );
+  return RenderButton();
+};
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
-
-export default Button
+export default Button;
