267c8183e926f6d56aa5b05116b5ab730a4387c7
Swarnali Bhattacharyya <oli2k3@gmail.com>
2025-01-13 20:13:32 +0530

    update title, add react-router-dom, and enhance Tailwind CSS configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-
 const App = () => {
   return (
-    <h1 className="text-5xl font-italic">
-      Hello world!!!!!!
-    </h1>
+    <>
+    <Navbar />
+    </>
   )
 }
 
-export default App
+export default App;
