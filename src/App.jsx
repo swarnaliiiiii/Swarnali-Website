ddc73c34b307a0a824e197ff6500ec804411017a
Swarnali Bhattacharyya <oli2k3@gmail.com>
2025-01-13 20:36:34 +0530

    add Button component and integrate with App; include gradient button in the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './components/Button'
 
 const App = () => {
   return (
     <>
     <h1 className='text-3xl'>Hello World</h1>
+    <div className='pt-[3.75rem] lg:pt-[5rem] overflow-hidden'>
+      <Button className="mt-10" href="#login">
+        Login
+      </Button>
+    </div>
+    <ButtonGradient />
     </>
   )
 }
