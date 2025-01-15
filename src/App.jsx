00f51152b1635e97634a6661da357fc4d0afdcaa
Swarnali Bhattacharyya <oli2k3@gmail.com>
2025-01-15 14:41:34 +0530

    add Header component and integrate it into App layout; remove commented-out code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Button from './components/Button'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <>
-    <h1 className='text-3xl'>Hello World</h1>
+    {/* <h1 className='text-3xl'>Hello World</h1> */}
     <div className='pt-[3.75rem] lg:pt-[5rem] overflow-hidden'>
-      <Button className="mt-10">
+      {/* <Button className="mt-10">
         Login
-      </Button>
+      </Button> */}
+      <Header />
     </div>
     <ButtonGradient />
     </>
