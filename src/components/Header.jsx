45a5ac5b6024e0bb91eb04dd030df0c2d334a68e
Swarnali Bhattacharyya <oli2k3@gmail.com>
2025-01-15 15:26:43 +0530

    enhance Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,15 @@ const Header = () => {
   return (
     <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-           <a>
-            <img src={brainwave}/>
+           <a className="block w-[12rem] xl:mr-8" href="#hero">
+            <img src={brainwave} width={180} height={40} alt="Brainwave"/>
            </a>
+           <nav className="hidden fixed top-[5rem] left-0 right-0 buttom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+             <div>
+                
+             </div>
+           </nav>
         </div>
-        header
     </div>
   )
 }
